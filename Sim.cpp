154328eee8734b15d195c38e685285616c92705d
gbrito11 <a2023144694@isec.pt>
2025-10-29 19:28:41 +0000

    implementação dos comandos possiveis

--- Sim.cpp
+++ Sim.cpp
@@ -30,6 +30,7 @@ void Sim::processarComando(const std::string& linha) {
     std::string cmd;
     ss >> cmd;
 
+    //comandos do jardim
     if (cmd == "jardim") {
         int linhas, colunas;
         if (ss >> linhas >> colunas)
@@ -37,33 +38,190 @@ void Sim::processarComando(const std::string& linha) {
         else
             std::cout << "Uso: jardim <linhas> <colunas>\n";
 
-    } else if (cmd == "planta") {
-        std::string pos;
-        char tipo;
-        if (ss >> pos >> tipo)
-            cmdPlanta(pos, tipo);
-        else
-            std::cout << "Uso: planta <pos> <tipo>\n";
 
+    }
+    //////////////////////////////////////
+    //Relacao com plantas-----------------------
+    ////////////////////////
+    else if (cmd == "planta") {
+        std::string pos;
+        std::string tipo;
+
+        if (ss >> pos >> tipo) {
+            if (pos.size() != 2) {
+                std::cout << "Uso: planta <posição> <tipo> (ex: planta FB F)\n";
+            }
+            else {
+                char linha = pos[0];
+                char coluna = pos[1];
+                cmdPlanta(linha,coluna, tipo);
+            }
+        }
+        else {
+            std::cout << "Uso: planta <posição> <tipo>\n";
+        }
     } else if (cmd == "colhe") {
         std::string pos;
-        if (ss >> pos)
-            cmdColhe(pos);
-        else
+        if (ss >> pos ) {
+            if (pos.size() != 2) {
+                std::cout << "Uso: Colher <posição> (ex: planta FB )\n";
+            }
+            else {
+                char linha = pos[0];
+                char coluna = pos[1];
+                cmdColhe(linha,coluna);
+            }
+        }else
             std::cout << "Uso: colhe <pos>\n";
 
-    } else if (cmd == "avanca") {
+
+    }
+/////////////////////////////////////////////////////////
+    // COMANDOS FERRAMENTAS
+////////////////////////////////////////////////////
+
+    else if (cmd == "larga") {
+        std::cout << "A largar ferramenta...\n";
+        cmdLarga();
+    }else if (cmd == "compra") {
+        char tipo;
+        ss>>tipo;
+        std::cout << "A comprar ferramenta...\n";
+        if(tipo== 'g' || tipo == 'a' || tipo == 't' || tipo == 'z') {
+            cmdCompraFerramenta(tipo);
+        }
+    }
+/////////////////////////////////////////////////////
+///Movimentso do Jardineiro
+////////////////////////////////////////
+    else if (cmd == "e" || cmd== "d" || cmd == "b" || cmd == "c") {
+        char mov= cmd[0];
+        switch (mov) {
+            case 'e':
+                cmdEsquerda();
+                break;
+            case 'd':
+                cmdDireita();
+                break;
+            case 'b':
+                cmdBaixo();
+                break;
+            case 'c':
+                cmdCima();
+            default:
+                std::cout << "MOvimento inválido!\n";
+                break;
+
+        }
+    }
+
+    else if (cmd== "entra") {
+        std::string pos;
+        if (ss >> pos ) {
+            if (pos.size() != 2) {
+                std::cout << "Uso: Entra <posição> (ex: planta FB )\n";
+            }
+            else {
+                char linha = pos[0];
+                char coluna = pos[1];
+                cmdEntra(linha,coluna);
+            }
+        }else
+            std::cout << "Uso: entra <pos>\n";
+
+    }else if (cmd == "sai") {
+        cmdSai();
+
+    }
+///////////////////////////////////////////////////////////////77
+///Comandos informacao ao utilizador
+///////////////////////////////////////////////////////////////
+    else if (cmd == "Lplantas") {
+        std::cout<<"Informação de todas as plantas no jardim:\n";
+        cmdLPlantas();
+    }else if (cmd == "Lplanta") {
+        std::string pos;
+        if (ss >> pos ) {
+            if (pos.size() != 2) {
+                std::cout << "Uso: Lplanta <posição> (ex: planta FB )\n";
+            }
+            else {
+                char linha = pos[0];
+                char coluna = pos[1];
+                cmdLPlanta(linha,coluna);
+            }
+        }else {
+            std::cout << "Uso: Lplanat <pos>\n";
+        }
+    }else if (cmd == "Larea") {
+        std::cout<<"Informações sobre o jardim :\n";
+        cmdLArea();
+    }else if (cmd == "Lsolo") {
+        std::string pos;
+        int n = 0;
+
+        if (ss >> pos) {
+            char linha, coluna;
+
+            if (pos.size() != 2) {
+                std::cout << "Uso: Lsolo <posição> [<n>]\n";
+                return;
+            }
+
+            linha = pos[0];
+            coluna = pos[1];
+
+            // tenta ler um número extra
+            if (ss >> n) {
+                cmdLSolo(linha, coluna, n);
+            } else {
+                cmdLSolo(linha, coluna);
+            }
+        }
+        else {
+            std::cout << "Uso: Lsolo <posição> [<n>]\n";
+        }
+    }else if (cmd == "Lferr") {
+        std::cout<<"Ferramentas do Jardineiro:\n";
+        cmdLferr();
+    }
+
+
+
+    else if (cmd == "avanca") {
         int n = 1;
         ss >> n;
         cmdAvanca(n);
 
-    } else if (cmd == "sai") {
-        cmdSai();
-
     } else if (cmd == "fim") {
         cmdFim();
 
-    } else {
+    }else if (cmd == "grava") {
+        std::string nome;
+        if (ss >> nome ) {
+            std::cout << "Jogo gravado com o nome: " << nome << ".\n";
+            cmdGrava(nome);
+        }
+    }else if (cmd == "recupera") {
+        std::string nome;
+        if (ss >> nome) {
+            std::cout << "Jogo recuperado com o nome: " << nome << ".\n";
+            cmdRecupera(nome);
+        }
+    }else if (cmd == "apaga") {
+        std::string nome;
+        if (ss >> nome) {
+            std::cout << "Jogo apagado com o nome: " << nome << ".\n";
+            cmdApaga(nome);
+        }
+    }else if (cmd == "executa") {
+        std::string nome;
+        if (ss >> nome) {
+            std::cout << "Jogo executado com o nome: " << nome << ".\n";
+            cmdExecuta(nome);
+        }
+    }
+     else {
         std::cout << "Comando desconhecido.\n";
     }
 }
@@ -75,31 +233,109 @@ void Sim::cmdJardim(int l,int c) {
     jardim->mostrar();
 }
 
-void Sim::cmdPlanta(const std::string& pos, char tipo) {
+
+// PLantas---------------------------------------
+void Sim::cmdPlanta( char l, char c , std::string tipo) {
     if (!jardim) {
         std::cout << "Erro: ainda não existe um jardim.\n";
         return;
     }
-    std::cout << "Plantar tipo '" << tipo << "' na posição " << pos << ".\n";
+    std::cout << "Plantar tipo '" << tipo << "' na posição : <" << l <<"> <"<< c << ">" ".\n";
     // adicioanr a planta
 }
 
-void Sim::cmdColhe(std::string pos) {
-    std::cout << "Colher planta na posição " << pos << ".\n";
+void Sim::cmdColhe(char l, char c) {
+    std::cout << "Colher planta na posição " << l << ","<< c << ".\n";
     // Aqui vais chamar jardim->colherPlanta(pos)
 }
 
+// Ferramentas--------------------------------------------------------
+void Sim::cmdLarga() {
+    std::cout << "A largar ferramenta...\n";
+    // chama para largar a ferramenta
+}
+void Sim::cmdCompraFerramenta(char tipo) {
+    std::cout << "A comprar ferramenta...\n";
+    // chama para comprar a ferramenta
+}
+
+//MOvimentso do jardineiro-----------------------------
+void cmdEsquerda() {
+    //esquerda
+}
+void cmdDireita() {
+    //direita
+}
+void cmdCima() {
+    //cima
+}
+void cmdBaixo() {
+    //baixo
+}
+
+void cmdEntra(char l,char c) {
+    std::cout<<"Jardineiro vai entrar na posição <"<<l<<"> <"<< c<<">" << std::endl;
+    //implementar o jardineiro a esntrar
+}
+
+void cmdSai() {
+    std::cout<<"Jardineiro a sair do jardim..."<<std::endl;
+}
+
+//Comandos informacao ao utilizador------------------------------------------------------
+
+void Sim::cmdLPlantas() {
+    std::cout << "Informação de todas as plantas no jardim.\n";
+    // listar todas as plantas
+}
+void Sim::cmdLPlanta(char l,char c) {
+    std::cout << "Informação da planta na posição " << l << "," << c << ".\n";
+    // mostrar info da planta na posicao
+}
+void Sim::cmdLArea() {
+    std::cout << "Informação da área do jardim.\n";
+}
+
+void Sim::cmdLSolo(char l, char c) {
+    // código para 1 célula
+}
+
+void Sim::cmdLSolo(char l, char c, int n) {
+    // código para n células
+}
+
+void Sim::cmdLferr() {
+    std::cout << "Informação da ferramenta do jardineiro.\n";
+    // mostrar info da ferramenta
+}
+//COmados especificos-------------------------------------------
 void Sim::cmdAvanca(int n) {
     std::cout << "Avançar " << n << " instantes.\n";
     // chama para avancar o tempo
 }
 
-void Sim::cmdSai() {
-    std::cout << "Jardineiro saiu do jardim.\n";
-    // jadrineiro sai do jardim
-}
-
 void Sim::cmdFim() {
     std::cout << "A terminar simulação...\n";
     emExecucao = false;
+}
+
+
+void Sim::cmdGrava(std::string jogo){
+    std::cout<<"A gravar jogo..."<<std::endl;
+    //GRavar o jogo
+}
+
+void Sim::cmdRecupera(std::string jogo) {
+    std::cout<<"A recuperar jogo..."<<std::endl;
+    //REcuperar o jogo
+}
+
+void Sim::cmdApaga(std::string jogo) {
+    std::cout<<"A apagar jogo..."<<std::endl;
+    //Apagar o jogo
+}
+
+void Sim::cmdExecuta(std::string comandos) {
+    std::cout<<"A executar comandos..."<<std::endl;
+    //executa comandos de um ficheiro para uo jogo
 }
--- Sim.h
+++ Sim.h
@@ -13,6 +13,7 @@ private:
     Jardim* jardim = nullptr;
     Jardineiro* jardineiro = nullptr;
     bool emExecucao = true;
+    int instantes;
 
 public:
     Sim();
@@ -21,13 +22,38 @@ public:
     void run();                // loop principal
     void processarComando(const std::string& linha);
 
-    // comandos específicos
+    // comandos Jardim/PLantas
     void cmdJardim(int l,int c);
-    void cmdPlanta(const std::string& pos, char tipo);
-    void cmdColhe(std::string pos);
-    void cmdAvanca(int n);
+    void cmdPlanta( char l,char c, std::string tipo);
+    void cmdColhe(char l, char c);
+
+    //COmandos ferramentas
+    void cmdLarga();
+    void cmdCompraFerramenta(char tipo);
+
+    //Moviemtos
+    void cmdEsquerda();
+    void cmdDireita();
+    void cmdCima();
+    void cmdBaixo();
+    void cmdEntra(char l,char c);
     void cmdSai();
+
+    //Comandos de Informacao ao utilizador
+    void cmdLPlantas();
+    void cmdLPlanta(char l,char c);
+    void cmdLArea();
+    void cmdLSolo(char l,char c);
+    void cmdLSolo(char l, char c, int n);
+    void cmdLferr();
+
+    //Comandos diversos
+    void cmdAvanca(int n);
     void cmdFim();
+    void cmdGrava(std::string jogo);
+    void cmdRecupera(std::string jogo);
+    void cmdApaga(std::string jogo);
+    void cmdExecuta(std::string comandos);
 };
 
 #endif
